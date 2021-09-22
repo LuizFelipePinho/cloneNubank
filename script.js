4f6f2f4ebf183865a692788251cff1baac00e5e9
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-09-21 23:44:16 -0300

    comentarios adicionais

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// simula nosso banco de dados
 const dbUsuarios = [
     {
         cpf: "123",
@@ -5,17 +6,22 @@ const dbUsuarios = [
     }
 ]
 
-
+// pega o botao de continuar
 const botaoContinuar = document.querySelector('.botaoContinuar')
+
+// adiciona um evento de clique, que chama uma função  
 botaoContinuar.addEventListener('click', () => {
     
+    //seleciona o campo de input e pega o valor que foi digitado nele
     const pegaCpf = document.querySelector('#cpf');
     const valorCpf = pegaCpf.value
 
+    //seleciona o campo de input e pega o valor que foi digitado nele
     const pegaSenha = document.querySelector('#senha');
     const valorSenha = pegaSenha.value
 
 
+    // monta o modelo que é exatamente ao do banco
     const montaUsuario = (cpf, senha) => {
         const usuario = {
             cpf: cpf,
@@ -26,6 +32,7 @@ botaoContinuar.addEventListener('click', () => {
 
     console.log(montaUsuario(valorCpf, valorSenha))
 
+    // procura dentro do pseudo banco se ele existe
     const temBD = dbUsuarios.map( (elemento) => {
         if(elemento.cpf == valorCpf && elemento.senha == valorSenha){
             return true
@@ -34,6 +41,7 @@ botaoContinuar.addEventListener('click', () => {
         }
     })
 
+    // mostra se existe o usuario ou nao
     if(temBD[0] == true) {
         alert('sucesso')
     } else{
